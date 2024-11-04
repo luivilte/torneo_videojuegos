35d261d0ac79b3cd369e8090590a6de16c10c3de
luivilte <143047049+luivilte@users.noreply.github.com>
2024-11-04 11:15:45 -0300

    Update ScheduleSection.jsx

--- torneo-videojuegos/src/secciones/Grupo 3 (Axel y Luis)/ScheduleSection.jsx
+++ torneo-videojuegos/src/secciones/Grupo 3 (Axel y Luis)/ScheduleSection.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
 
 const ScheduleSection = () => {
+  /* Lógica de JavaScript */
+  
   return (
+    /* Estructura del HTML (Elementos visuales de las web) */
     <div className='text-white' >
       ScheduleSection
     </div>
